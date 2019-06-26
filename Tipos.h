194299ab7381a649371717b74fce20172e244901
Amdaruh <adrian.ojeda@utec.edu.pe>
2019-06-26 02:45:25 -0500

    Proyecto 2 de poo

---
+++ Tipos.h
@@ -0,0 +1,20 @@
+//
+// Created by Adrian on 26/06/2019.
+//
+
+#ifndef UNTITLED3_TIPOS_H
+#define UNTITLED3_TIPOS_H
+
+#endif //UNTITLED3_TIPOS_H
+
+#ifndef AGREGACION_TIPOS_H
+#define AGREGACION_TIPOS_H
+
+#include <string>
+using namespace std;
+// Definiendo alias
+using TipoEntero = int;
+using TipoCaracter = char;
+using TipoString = string;
+
+#endif //AGREGACION_TIPOS_H
---
+++ cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Adrian/untitled3/Menu.cpp" "C:/Users/Adrian/untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/Menu.cpp.obj"
+  "C:/Users/Adrian/untitled3/Objeto.cpp" "C:/Users/Adrian/untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/Objeto.cpp.obj"
+  "C:/Users/Adrian/untitled3/Tierra.cpp" "C:/Users/Adrian/untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/Tierra.cpp.obj"
+  "C:/Users/Adrian/untitled3/main.cpp" "C:/Users/Adrian/untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
